fa3d6f5bac18ab1e2cb9f79148de24c1637ca7eb
C0c0Goddess <precious.bagby@gmail.com>
2026-06-06 06:45:17 -0700

    Delete lol_wedding_system_full directory

--- lol_wedding_system_full/client/animations.lua
+++
@@ -1,9 +0,0 @@
-WeddingAnimations = {}
-
-WeddingAnimations.Kiss = function()
-    print('Play kiss animation')
-end
-
-WeddingAnimations.ExchangeRings = function()
-    print('Play ring exchange animation')
-end
